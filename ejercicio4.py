fdbf1f69d2d681153cd163d226c01b71b0043739
David Ruiz <113980722+davidruiiz@users.noreply.github.com>
2022-12-18 23:20:14 +0100

    cambios

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,24 +1,14 @@
-
-
-
-
-def luck_check(lista):
-    
-    suma = 0
-
-    for i in range(len(lista)):
-
-        if lista[i] == 7:
-
-            suma += 14
-
-        else:
-
-            suma += lista[i]
-
-    return suma
-
-if __name__=='__main__':
-    print(luck_check([5, 7, 7]))
-    print(luck_check([5, 7, 1, 8]))
-    
+# En algunos países de la antigua Unión Soviética existía la creencia de los boletos de la
+# suerte. Se creía que un billete de transporte de cualquier tipo traía suerte si la suma de los
+# dígitos de la mitad izquierda de su número era igual a la suma de los dígitos de la mitad
+# derecha. Aquí hay ejemplos de tales números:
+# 003111 # 3 = 1 + 1 + 1
+# 813372 # 8 + 1 + 3 = 3 + 7 + 2
+# 17935 # 1 + 7 = 3 + 5 // si la longitud es impar, debes ignorar el número del medio al
+# sumar las mitades.
+# 56328116 # 5 + 6 + 3 + 2 = 8 + 1 + 1 + 6
+# Dichos boletos se comían después de usarlos o se recolectaban para fanfarronear.
+# Su tarea es escribir una función luck_check(str), que devuelve true/Truesi el argumento es una
+# representación decimal de cadena de un número de boleto de la suerte, o false/Falsepara
+# todos los demás números. Debería arrojar errores para cadenas vacías o cadenas que no
+# representan un número decimal.
