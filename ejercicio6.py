45aa2abde84220f2d51e3a7ef2d5bba2591b1f5f
David Ruiz <113980722+davidruiiz@users.noreply.github.com>
2022-12-18 23:30:13 +0100

    Update ejercicio6.py

--- ejercicio6.py
+++ ejercicio6.py
@@ -15,3 +15,33 @@
 # [[3, 6, 9],
 #  [2, 5, 8],
 #  [1, 4, 7]]
+
+import numpy as np  
+
+def pedir_numero():
+    while True:
+        try:
+            n = int(input("Introduce un número para determinar el orden de la matriz: "))
+            return n
+        except ValueError:
+            print("Error. Introduce un número entero")
+
+def matriz(n):
+    return np.random.randint(1, 100, size=(n, n))
+
+def rotar_clockwise(matriz):
+    return np.rot90(matriz)
+
+def rotar_anticlockwise(matriz):
+    matrix1 = np.rot90(matriz)
+    matrix2 = np.rot90(matrix1)
+    matrix3 = np.rot90(matrix2)
+    return matrix3
+
+
+if __name__ == '__main__':
+    n = pedir_numero()
+    matrix = matriz(n)
+    print(matrix, '\n')
+    print(rotar_clockwise(matrix), '\n')
+    print(rotar_anticlockwise(matrix))
